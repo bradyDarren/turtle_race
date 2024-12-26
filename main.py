a8ae24c7883ddae5ac67db301f164f075069b0d7
bradyDarren <dcgbfinancial@gmail.com>
2024-12-26 06:10:54 -0600

    initital commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+screen = Screen()
+screen.setup(width=500,height=400)
+screen.textinput("Turtle Race", "Guess which color turtle will win the race.")
+
+screen.exitonclick()
