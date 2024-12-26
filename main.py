a5cbff2fcd6a605447183f3c9e9fcd54cbd1479a
bradyDarren <dcgbfinancial@gmail.com>
2024-12-26 06:29:50 -0600

    addition of for loop

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from turtle import Turtle, Screen
+import random
 
-timmy = Turtle()
 
 screen = Screen()
 screen.setup(width=500,height=400)
 screen.textinput("Turtle Race", "Guess which color turtle will win the race.")
+
+timmy = Turtle()
+tom = Turtle()
+thomas = Turtle()
+todd = Turtle()
+tony = Turtle()
+theo = Turtle()
+
+turtles = [timmy,tom,thomas, todd, tony, theo]
+colors = ["red", "blue", "green", "yellow", "orange", "purple"]
+y_cord = 0
+
+for turtle in turtles:
+    turtle.shape("turtle")
+    turtle.color(random.choice(colors))
+    turtle.goto(x=-240, y=y_cord)
+    y_cord += 20
+
+# timmy.goto(x=-240,y=0)
 
 screen.exitonclick()
